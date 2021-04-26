15b9080374e296b5cb8d216dfd0e31a670ee4475
pirgan <pirgan@gmail.com>
2021-04-26 13:25:41 +0200

    JWT Strategy-1

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer fixed v-model="drawer" app>
+      <v-list-dense>
+        <router-link v-bind:to="{ name: 'Home' }" class="side_bar_link">
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>Home</v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+        <router-link v-bind:to="{ name: 'Contact' }" class="side_bar_link">
+          <v-list-tile>
+            <v-list-tile-action>
+             <v-icon>contact_mail</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>Contact</v-list-tile-content>
+          </v-list-tile>
+        </router-link>
+      </v-list-dense>
+    </v-navigation-drawer>
+  </v-app>
 </template>
 
 <script>
+import './assets/stylesheets/main.css';
+
 export default {
   name: 'App',
 };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
